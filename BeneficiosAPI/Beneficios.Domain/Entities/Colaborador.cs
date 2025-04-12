59943cababbf2957d773ee3a44d88f5174f3615c
Rafael de Oliveira Gomes <rafinha.gomo.11.og@gmail.com>
2025-04-11 21:10:10 -0300

    Adiciona sistema de notificações e classes de domínio
    Implementa novas classes como Beneficio, Colaborador e
    ColaboradorBeneficioViewModel. Atualiza o projeto para incluir
    MediatR e organiza a estrutura de namespaces. A classe Response
    encapsula dados de resposta e notificações de erro.

---
+++ BeneficiosAPI/Beneficios.Domain/Entities/Colaborador.cs
@@ -0,0 +1,10 @@
+﻿namespace Beneficios.Domain.Entities;
+
+public class Colaborador
+{
+    public long IdColaborador { get; set; }
+    public string? Nome { get; set; }
+    public string? Cargo { get; set; }
+    public List<Beneficio>? Beneficios { get; set; }
+    
+}
---
+++ BeneficiosAPI/Beneficios.Domain/Entities/ViewModel/ColaboradorBeneficioViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Beneficios.Domain.Entities.ViewModel;
+
+public class ColaboradorBeneficioViewModel
+{
+    public ColaboradorBeneficioViewModel() { }
+
+    public ColaboradorBeneficioViewModel(Colaborador colaborador, List<Beneficio>? beneficios)
+    {
+        Colaborador!.IdColaborador = colaborador.IdColaborador;
+        Colaborador.Nome = colaborador.Nome;
+        Colaborador.Cargo = colaborador.Cargo;
+        Beneficios = beneficios;
+    }
+
+    public Colaborador Colaborador { get; set; }
+    public IEnumerable<Beneficio>? Beneficios { get; set; }
+}
